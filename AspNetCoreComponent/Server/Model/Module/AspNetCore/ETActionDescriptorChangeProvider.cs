b53a816e56431e254c595dc836e724f4fd41b66c
夏 <a>
2019-03-29 19:16:56 +0800

    添加http组件

---
+++ AspNetCoreComponent/Server/Model/Module/AspNetCore/ETActionDescriptorChangeProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Primitives;
+using System.Threading;
+
+namespace ETModel.Module.AspNetCore
+{
+    public class ETActionDescriptorChangeProvider : IActionDescriptorChangeProvider
+    {
+        public static ETActionDescriptorChangeProvider Instance { get; } = new ETActionDescriptorChangeProvider();
+
+        public CancellationTokenSource TokenSource { get; private set; }
+
+        public bool HasChanged { get; set; }
+
+        public IChangeToken GetChangeToken()
+        {
+            TokenSource = new CancellationTokenSource();
+            
+            return new CancellationChangeToken(TokenSource.Token);
+        }
+    }
+}
---
+++ AspNetCoreComponent/Server/Model/Module/AspNetCore/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ETModel.Module.AspNetCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                    .AddApplicationPart(Game.EventSystem.Get(DLLType.Hotfix))
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IActionDescriptorChangeProvider>(ETActionDescriptorChangeProvider.Instance);
+
+            services.AddSingleton(ETActionDescriptorChangeProvider.Instance);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
