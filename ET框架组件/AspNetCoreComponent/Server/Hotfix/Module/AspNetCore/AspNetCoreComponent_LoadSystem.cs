42cd0ac8f34dd2bd5b241aed29321f247d81fc95
公司 <434635474@qq.com>
2019-07-03 14:40:35 +0800

    修改目录结构

---
+++ ET框架组件/AspNetCoreComponent/Server/Hotfix/Module/AspNetCore/AspNetCoreComponent_LoadSystem.cs
@@ -0,0 +1,24 @@
+﻿using ETModel;
+using ETModel.Module.AspNetCore;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ETHotfix.Module.AspNetCore
+{
+    [ObjectSystem]
+    public class AspNetCoreComponent_LoadSystem : LoadSystem<AspNetCoreComponent>
+    {
+        public override void Load(AspNetCoreComponent self)
+        {
+            ApplicationPartManager partManager = self.ServiceProvider.GetService<ApplicationPartManager>();
+
+            partManager.ApplicationParts.Clear();
+
+            partManager.ApplicationParts.Add(new AssemblyPart(Game.EventSystem.Get(DLLType.Hotfix)));
+
+            ETActionDescriptorChangeProvider.Instance.HasChanged = true;
+
+            ETActionDescriptorChangeProvider.Instance.TokenSource.Cancel();
+        }
+    }
+}
